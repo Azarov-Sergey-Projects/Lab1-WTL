09b35a01d1a0899b8ad2e0a51ac396bd6acf909a
Огурчик <sergeyazarov1006@gmail.com>
2021-03-28 19:34:32 +0300

    #3

--- MyDialogBar.h
+++ MyDialogBar.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <vector>
 #include <thread>
 
 #include "resource.h"
@@ -28,7 +27,7 @@ public:
 			OnCloseCmd(uMsg,wParam,lParam,bHandled);
 			return 0;
 		case IDC_BUTTON_APPLY:
-			ListView_DeleteAllItems(myListView.m_hWnd);
+			ListView_DeleteAllItems(this->myListView.m_hWnd);
 			if (FindThread.joinable())
 			{
 				FindThread.detach();
@@ -48,8 +47,7 @@ public:
 		}
 		return 0;
 	}
-
-
+	
 	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
 	{
 		myListView.Create(m_hWnd);
@@ -66,116 +64,10 @@ public:
 		return 0;
 	}
 
-	/*void Find(CString szPath)
-	{
-		CFindFile F;
-		CString S = szPath+ TEXT("\\*.*");
-		
-		BOOL  bFlag = F.FindFile(S);
-		while (bFlag)
-		{
-			bFlag = F.FindNextFile();
-			if (F.IsDirectory() == TRUE&&F.IsDots()==FALSE)
-			{
-				myListView.InsertItem(0, F.GetFileName());
-				myListView.SetItemText(0, 2,F.GetFilePath());
-				Find(F.GetFilePath());
-			}
-		}
-		F.Close();
-	}*/
-	/*void FindFile(CString szPath)
-	{
-		CFindFile F;
-		CString S = szPath+TEXT("\\*.*");
-		i = 0;
-		BOOL bFlag = F.FindFile(S);
-		if (!bFlag)
-		{
-			MessageBox(TEXT("Error"), TEXT("File not found"), 0);
-		}
-		else
-		{
-			do
-			{
-				if (F.IsDirectory() == TRUE && F.IsDots() == FALSE)
-				{
-					View_List(F.GetFileName(), i);
-					i++;
-					FindFile(F.GetFilePath());
-				}
-				else
-				{
-					View_List(F.GetFileName(), i);
-					i++;
-				}
-			
-			} while (F.FindNextFileW());
-			F.Close();
-			InitListViewImage(i, S);
-		}
-	}
-	void View_List(CString buf, int i)
-	{
-		LVITEM lvItem;
-		lvItem.mask = LVIF_IMAGE | LVIF_TEXT;
-		lvItem.state = 0;
-		lvItem.stateMask = 0;
-		lvItem.iItem = i;
-		lvItem.iImage = i+2;
-		lvItem.iSubItem = 0;
-		lvItem.pszText = (LPWSTR)buf.GetString();
-		lvItem.cchTextMax = wcslen(buf);
-		myListView.InsertItem(&lvItem);
-	}
-
-	BOOL InitListViewImage(int size, CString path)
-	{
-		CFindFile F;
-		HIMAGELIST hSmall;
-		SHFILEINFO lp;
-		hSmall = ImageList_Create(GetSystemMetrics(SM_CXSMICON), GetSystemMetrics(SM_CYSMICON), ILC_MASK | ILC_COLOR32, size, 1);
-		bool hFind = F.FindFile(path);
-		if (!hFind)
-		{
-			MessageBox(TEXT("Error"), TEXT("File not found"), MB_OK | MB_ICONWARNING);
-		}
-		else
-		{
-			do
-			{
-				if (wcscmp(F.GetFileName(), TEXT("."))==0)
-				{
-					SHGetFileInfo(TEXT(""), FILE_ATTRIBUTE_DEVICE, &lp, sizeof(&lp), SHGFI_ICONLOCATION | SHGFI_ICON | SHGFI_SMALLICON);
-					ImageList_AddIcon(hSmall, lp.hIcon);
-					DestroyIcon(lp.hIcon);
-				}
-				if (wcscmp(F.GetFileName(),TEXT("..")) == 0)
-				{
-					SHGetFileInfo(TEXT(""), FILE_ATTRIBUTE_DIRECTORY, &lp, sizeof(lp), SHGFI_ICONLOCATION | SHGFI_ICON | SHGFI_SMALLICON);
-					ImageList_AddIcon(hSmall, lp.hIcon);
-					DestroyIcon(lp.hIcon);
-				}
-				path.Delete(path.GetLength() - 3, 3);
-				CString FileInfo = path+ F.GetFileName();
-				wchar_t buf1[MAX_PATH] = L"C:\\Users\\Огурчик\\Desktop\\";
-				wcscat(buf1, F.GetFileName());
-
-				DWORD num = GetFileAttributes(buf1);
-				SHGetFileInfoW(buf1, num, &lp, sizeof(lp), SHGFI_SYSICONINDEX | SHGFI_ICON | SHGFI_USEFILEATTRIBUTES);
-				ImageList_AddIcon(hSmall, lp.hIcon);
-				DestroyIcon(lp.hIcon);
-			} while (F.FindNextFileW());
-			F.Close();
-		}
-		myListView.SetImageList(hSmall, LVSIL_SMALL);
-		return TRUE;
-	}*/
+	
 private:
 	CString FileName;
 	CString FileExtention;
 	MyListView myListView;
-	//CListViewCtrl myListView;
 	std::thread FindThread;
-	int i;
 };
--- MyListView.h
+++ MyListView.h
@@ -1,8 +1,7 @@
 #pragma once
-
 #include <atlbase.h>
-#include <atlmisc.h>
 #include <atlapp.h>
+#include <atlmisc.h>
 #include <atlwin.h>
 #include <atlctrls.h>
 #include <atlfile.h>
@@ -13,105 +12,13 @@
 class MyListView:public CListViewCtrl
 {
 private:
-	CListViewCtrl myListView;
+	LVITEM lvItem;
+	CListViewCtrl myListView; 
 	CString path;
 	int i;
 public:
-	void Create(HWND m_hWnd)
-	{
-		CRect myRect{ 0,0,600,400 };
-		//CenterWindow(GetParent());
-		myListView.Create(m_hWnd, myRect, NULL, WS_CHILD|WS_VISIBLE | WS_CLIPSIBLINGS | WS_CLIPCHILDREN |
-				LVS_REPORT | LVS_AUTOARRANGE | LVS_SHOWSELALWAYS | LVS_SHAREIMAGELISTS);
-		myListView.InsertColumn(0, TEXT("Название"), LVCFMT_LEFT, 290);
-		myListView.InsertColumn(1, TEXT(".*"), LVCFMT_LEFT, 50);
-		myListView.InsertColumn(2, TEXT("Полный путь"), LVCFMT_LEFT, 290);
-	}
-	void FindFile(CString szPath)
-	{
-		CFindFile F;
-		CString S = szPath + TEXT("\\*.*");
-		i = 0;
-		BOOL bFlag = F.FindFile(S);
-		if (!bFlag)
-		{
-			MessageBox(TEXT("Error"), TEXT("File not found"), 0);
-		}
-		else
-		{
-			do
-			{
-				if (F.IsDirectory() == TRUE && F.IsDots() == FALSE)
-				{
-					View_List(F.GetFileName(), i);
-					i++;
-					FindFile(F.GetFilePath());
-				}
-				else
-				{
-					View_List(F.GetFileName(), i);
-					i++;
-				}
-
-			} while (F.FindNextFileW());
-			F.Close();
-			InitListViewImage(i, S);
-		}
-	}
-	void View_List(CString buf, int i)
-	{
-		LVITEM lvItem;
-		lvItem.mask = LVIF_IMAGE | LVIF_TEXT;
-		lvItem.state = 0;
-		lvItem.stateMask = 0;
-		lvItem.iItem = i;
-		lvItem.iImage = i + 2;
-		lvItem.iSubItem = 0;
-		lvItem.pszText = (LPWSTR)buf.GetString();
-		lvItem.cchTextMax = wcslen(buf);
-		myListView.InsertItem(&lvItem);
-	}
-
-	BOOL InitListViewImage(int size, CString path)
-	{
-		CFindFile F;
-		HIMAGELIST hSmall;
-		SHFILEINFO lp;
-		hSmall = ImageList_Create(GetSystemMetrics(SM_CXSMICON), GetSystemMetrics(SM_CYSMICON), ILC_MASK | ILC_COLOR32, size, 1);
-		bool hFind = F.FindFile(path);
-		if (!hFind)
-		{
-			MessageBox( TEXT("Error"), TEXT("File not found"), MB_OK | MB_ICONWARNING);
-		}
-		else
-		{
-			do
-			{
-				if (wcscmp(F.GetFileName(), TEXT(".")) == 0)
-				{
-					SHGetFileInfo(TEXT(""), FILE_ATTRIBUTE_DEVICE, &lp, sizeof(&lp), SHGFI_ICONLOCATION | SHGFI_ICON | SHGFI_SMALLICON);
-					ImageList_AddIcon(hSmall, lp.hIcon);
-					DestroyIcon(lp.hIcon);
-				}
-				if (wcscmp(F.GetFileName(), TEXT("..")) == 0)
-				{
-					SHGetFileInfo(TEXT(""), FILE_ATTRIBUTE_DIRECTORY, &lp, sizeof(lp), SHGFI_ICONLOCATION | SHGFI_ICON | SHGFI_SMALLICON);
-					ImageList_AddIcon(hSmall, lp.hIcon);
-					DestroyIcon(lp.hIcon);
-				}
-				path.Delete(path.GetLength() - 3, 3);
-				CString FileInfo = path + F.GetFileName();
-				wchar_t buf1[MAX_PATH] = L"C:\\Users\\Огурчик\\Desktop\\";
-				wcscat(buf1, F.GetFileName());
-
-				DWORD num = GetFileAttributes(buf1);
-				SHGetFileInfoW(buf1, num, &lp, sizeof(lp), SHGFI_SYSICONINDEX | SHGFI_ICON | SHGFI_USEFILEATTRIBUTES);
-				ImageList_AddIcon(hSmall, lp.hIcon);
-				DestroyIcon(lp.hIcon);
-			} while (F.FindNextFileW());
-			F.Close();
-		}
-		myListView.SetImageList(hSmall, LVSIL_SMALL);
-		return TRUE;
-	}
+	void Create(HWND m_hWnd);
+	void FindFile(CString szPath);
+	void View_List(CString buf, int i);
+	BOOL InitListViewImage(int size, CString path);
 };
